1ed2c341b3d9b391625b75fd2dbfb221d670f21a
Suji Marium Saji <85631667+suji-marium@users.noreply.github.com>
2023-08-20 20:52:51 +0530

    Create perfectsquare.c

---
+++ perfectsquare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int arr[100],n,count=0;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=arr[i];j++)
+        
+        {
+            if(j*j==arr[i])
+                count++;
+        }
+    }
+    printf("Number of perfect square is %d",count);
+}
